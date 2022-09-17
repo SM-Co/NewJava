a9cb46f69245ed2170dba2c4d737215ea898fbae
Soleman <leohbkbd@gmail.com>
2022-09-16 22:32:51 -0400

    4th commit

--- src/JavaClass.java
+++ src/JavaClass.java
@@ -1,7 +1,16 @@
+import java.sql.SQLOutput;
+
 public class JavaClass {
 
     public static void main (String[] args) {
 
         System.out.println("This is working");
     }
+
+    public static void bugFix {
+
+        System.out.println("This is a bug fix");
+    }
+
+
 }
